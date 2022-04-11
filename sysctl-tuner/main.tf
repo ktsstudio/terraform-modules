0f8b8736555c4d5c69d6891ce8944c27cc04e2bf
igorcoding <igorcoding@gmail.com>
2022-04-11 23:24:18 +0300

    simplify sysctl tuner

--- sysctl-tuner/main.tf
+++ sysctl-tuner/main.tf
@@ -1,83 +1,52 @@
-resource "kubernetes_service_account" "sa" {
+resource "kubernetes_daemonset" "sysctl-tuner" {
   metadata {
-    name      = var.sa_name
+    name      = "sysctl-tuner"
     namespace = var.namespace
-  }
-  automount_service_account_token = true
-}
-
-resource "kubernetes_cluster_role" "cr" {
-  metadata {
-    name = var.cluster_role_name
-  }
-
-  rule {
-    api_groups = ["*"]
-    resources  = ["*"]
-    verbs      = ["*"]
-  }
-
-  rule {
-    non_resource_urls = ["*"]
-    verbs             = ["*"]
-  }
-}
-
-resource "kubernetes_cluster_role_binding" "crb" {
-  metadata {
-    name = var.cluster_role_binding_name
-  }
-
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.cr.metadata[0].name
-  }
-
-  subject {
-    kind      = "ServiceAccount"
-    name      = kubernetes_service_account.sa.metadata[0].name
-    namespace = kubernetes_service_account.sa.metadata[0].namespace
-  }
-}
-
-resource "kubernetes_config_map" "configmap" {
-  metadata {
-    name      = "addon-operator"
-    namespace = kubernetes_service_account.sa.metadata[0].namespace
-  }
-
-  data = {
-    global             = ""
-    sysctlTunerEnabled = "true"
-    sysctlTuner        = <<EOT
-      params:
-        fs.inotify.max_user_instances: "8192"
-        fs.inotify.max_user_watches: "524288"
-      sleep: "300"
-    EOT
-  }
-}
-
-resource "kubernetes_pod" "operator" {
-  metadata {
-    name      = "sysctl-tuner-operator"
-    namespace = kubernetes_config_map.configmap.metadata[0].namespace
   }
 
   spec {
-    container {
-      name              = "sysctl-tuner-operator"
-      image             = var.image
-      image_pull_policy = "Always"
-
-      args = ["--namespace", kubernetes_config_map.configmap.metadata[0].namespace]
-
-      env {
-        name  = "RLOG_LOG_LEVEL"
-        value = "DEBUG"
+    strategy {
+      type = "RollingUpdate"
+    }
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name" = "sysctl-tuner"
       }
     }
-    service_account_name = kubernetes_service_account.sa.metadata[0].name
+    template {
+      metadata {
+        labels = {
+          "app.kubernetes.io/name" = "sysctl-tuner"
+        }
+      }
+      spec {
+        container {
+          name              = "sysctl-tuner"
+          image             = "busybox"
+          image_pull_policy = "Always"
+          command = [
+            "sh",
+            "-c",
+            <<EOT
+            while true; do
+                %{~for k, v in var.params}
+                sysctl -w ${k}=${v}
+                %{~endfor}
+                # More options are in http://www.brendangregg.com/blog/2017-12-31/reinvent-netflix-ec2-tuning.html
+                sleep ${var.sleep_seconds} ;
+            done
+            EOT
+          ]
+
+          security_context {
+            privileged = true
+          }
+        }
+
+        host_network = true
+        host_pid     = true
+        host_ipc     = true
+      }
+    }
   }
 }
--- sysctl-tuner/vars.tf
+++ sysctl-tuner/vars.tf
@@ -1,22 +1,12 @@
-variable "sa_name" {
-  type    = string
-  default = "sysctl-tuner-account"
-}
-
-variable "cluster_role_name" {
-  type    = string
-  default = "sysctl-tuner-account"
-}
-
-variable "cluster_role_binding_name" {
-  type    = string
-  default = "sysctl-tuner-role-binding"
-}
-
 variable "namespace" {
   type = string
 }
 
-variable "image" {
-  type = string
+variable "sleep_seconds" {
+  type    = number
+  default = 300
+}
+
+variable "params" {
+  type = map(string)
 }
