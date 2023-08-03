9ee782b08dede070ffb8e45920d87f207bc63505
Alex Egorov <a.egorov@ktsstudio.ru>
2023-08-03 14:48:57 +0300

    DEVOPS: Add cors creation in bucket creation YC

--- yc-bucket-with-owner/main.tf
+++ yc-bucket-with-owner/main.tf
@@ -30,4 +30,17 @@ resource "yandex_storage_bucket" "bucket" {
     type        = "CanonicalUser"
     permissions = ["READ", "WRITE"]
   }
+
+
+  dynamic "cors_rule" {
+    for_each = var.cors_enabled ? [1] : []
+
+    content {
+      allowed_headers = var.cors_allowed_headers
+      allowed_methods = var.cors_allowed_methods
+      allowed_origins = var.cors_allowed_origins
+      expose_headers  = var.cors_expose_headers
+      max_age_seconds = var.cors_max_age_seconds
+    }
+  }
 }
--- yc-bucket-with-owner/variables.tf
+++ yc-bucket-with-owner/variables.tf
@@ -15,3 +15,39 @@ variable "endpoint_url" {
   type    = string
   default = "https://storage.yandexcloud.net"
 }
+
+variable "cors_enabled" {
+  type        = bool
+  default     = false
+  description = "Activate adding of CORS rules"
+}
+
+variable "cors_allowed_headers" {
+  type        = list(string)
+  default     = []
+  description = "List of allowed headers for CORS rules"
+}
+
+variable "cors_allowed_methods" {
+  type        = list(string)
+  default     = []
+  description = "List of allowed methods for CORS rules"
+}
+
+variable "cors_allowed_origins" {
+  type        = list(string)
+  default     = []
+  description = "List of allowed origins for CORS rules"
+}
+
+variable "cors_expose_headers" {
+  type        = list(string)
+  default     = []
+  description = "List of allowed expose headers"
+}
+
+variable "cors_max_age_seconds" {
+  type        = number
+  default     = 0
+  description = "Max age of cors in seconds"
+}
